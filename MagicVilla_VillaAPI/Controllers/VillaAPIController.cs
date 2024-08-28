381e0728e1017e72343094be0ed2972119b31938
Md Al Amin <alamin.cse.ru2018@gmail.com>
2024-08-28 18:03:34 +0600

    VillaController and Villa Model created

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name="Pool View"},
+                new Villa { Id = 2, Name="Beach View"}
+            };
+        }
+    }
+}
